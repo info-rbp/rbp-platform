dafac8e32d01f6f38a75581692bb46b9b2604816
Gianpaulo Umberto COLETTI <info@remotebusinesspartner.com.au>
2026-05-15 22:47:35 +0800

    feat: add shared entitlements helper

---
+++ appwrite/functions/_shared/entitlements.ts
@@ -0,0 +1,17 @@
+export const entitlementKeys = [
+  "membership",
+  "portal",
+  "billing",
+  "notifications",
+  "offers",
+  "resources",
+  "documents",
+  "decision_desk",
+  "docushare",
+  "marketplace",
+  "connectivity",
+  "risk_advisor",
+  "fixer",
+  "applications_interest",
+  "applications_provisioning"
+] as const;
