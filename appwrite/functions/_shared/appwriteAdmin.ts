aa9ec6e285791ece6a45d71053e94d786ee34194
Gianpaulo Umberto COLETTI <info@remotebusinesspartner.com.au>
2026-05-15 22:46:43 +0800

    feat: add shared Appwrite admin helper

---
+++ appwrite/functions/_shared/appwriteAdmin.ts
@@ -0,0 +1,8 @@
+export function getRequiredAdminEnv() {
+  return {
+    endpoint: process.env.APPWRITE_ENDPOINT,
+    projectId: process.env.APPWRITE_PROJECT_ID,
+    apiKey: process.env.APPWRITE_API_KEY,
+    databaseId: process.env.APPWRITE_DATABASE_ID,
+  };
+}
